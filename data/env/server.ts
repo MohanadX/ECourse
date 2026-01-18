1c9aa785264ca1be43b7badfd1d368e8d435d327
Mohanad Ayoub <alaqlmhnd3@gmail.com>
2026-01-18 20:25:22 +0300

    removed docker postgres env

--- data/env/server.ts
+++ data/env/server.ts
@@ -3,12 +3,12 @@ import z from "zod";
 
 export const env = createEnv({
 	server: {
-		POSTGRES_PASSWORD: z
-			.string({ error: "DB Password is not set correctly" })
-			.min(1),
-		POSTGRES_USER: z.string({ error: "DB User is not set correctly" }).min(1),
-		POSTGRES_HOST: z.string({ error: "DB Host is not set correctly" }).min(1),
-		POSTGRES_DB: z.string({ error: "DB URL is not set correctly" }).min(1),
+		// POSTGRES_PASSWORD: z
+		// 	.string({ error: "DB Password is not set correctly" })
+		// 	.min(1),
+		// POSTGRES_USER: z.string({ error: "DB User is not set correctly" }).min(1),
+		// POSTGRES_HOST: z.string({ error: "DB Host is not set correctly" }).min(1),
+		// POSTGRES_DB: z.string({ error: "DB URL is not set correctly" }).min(1),
 		CLERK_WEBHOOK_SECRET: z.string().min(1),
 		IMAGEKIT_PUBLIC_KEY: z
 			.string()
