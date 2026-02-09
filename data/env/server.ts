cf502e493b431b156e359d99b1210b17f03f047d
Mohanad Ayoub <alaqlmhnd3@gmail.com>
2026-02-09 20:28:11 +0300

    Update data/env/server.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- data/env/server.ts
+++ data/env/server.ts
@@ -8,7 +8,7 @@ export const env = createEnv({
 			.min(1),
 		POSTGRES_USER: z.string({ error: "DB User is not set correctly" }).min(1),
 		POSTGRES_HOST: z.string({ error: "DB Host is not set correctly" }).min(1),
-		POSTGRES_DB: z.string({ error: "DB URL is not set correctly" }).min(1),
+		POSTGRES_DB: z.string({ error: "DB name is not set correctly" }).min(1),
 		CLERK_WEBHOOK_SECRET: z.string().min(1),
 		IMAGEKIT_PUBLIC_KEY: z
 			.string()
